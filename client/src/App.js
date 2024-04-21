69a96ab46e58c91cc93e895645b2dcc72c738d52
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2024-04-21 11:43:36 +0530

    created node modules

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Apna Homes</h1>
+      <button className="btn btn-primary"> button</button>
+
+    </div>
+  );
+}
+
+export default App;
