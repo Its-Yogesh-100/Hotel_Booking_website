0bf14ce5e48d5fcef35af296cc4fee5cbfa18d21
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2024-04-25 05:33:12 +0530

    addded various things

---
+++ db.js
@@ -0,0 +1,21 @@
+
+const mongoose = require("mongoose");
+
+const connectionURI = "mongodb+srv://yogesh:root@cluster0.obzycwr.mongodb.net/mern-rooms/";
+
+mongoose.connect(connectionURI);
+
+var connection = mongoose.connection;
+
+connection.on('error', (err) => {
+    console.error('Mongo DB connection failed:', err);
+});
+
+connection.on('connected', () => {
+    console.log('MongoDB connection connected');
+});
+
+module.exports = mongoose;
+
+
+
