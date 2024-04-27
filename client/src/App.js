603e17071674b6631db0b7f16ae5346773ad5d7d
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2024-04-27 20:00:05 +0530

    some more changes in project added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,45 @@
 
+// import './App.css';
+// import Navbar from './components/Navbar';
+// import {BrowserRouter ,Route, Link} from 'react-router-dom'
+// import Homescreen from './screens/Homescreen';
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <Navbar/>
+
+//     <BrowserRouter>
+
+//   <Route path="/home" exact Component={Homescreen}/>
+
+
+//     </BrowserRouter>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter ,Route,Routes, Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes component
 import Homescreen from './screens/Homescreen';
+// import Room from './components/Room';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-
-    <BrowserRouter>
-
-  <Routes>
-  <Route path='/home' exact Component={Homescreen}/>
-  </Routes>
-    
-
-  
-
-    
-    
-    
-    </BrowserRouter>
-
+      <Navbar />
+      <BrowserRouter>
+        <Routes> {/* Wrap your Routes in BrowserRouter */}
+          <Route path="/home" element={<Homescreen />} /> {/* Use element prop to render component */}
+          {/* <Route path='/rooms' element={} /> {/* Use element prop */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
-
